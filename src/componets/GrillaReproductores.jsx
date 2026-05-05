84600fa0d801159f462f6350327b784d4e379129
joaquinsosa00 <joaquin150496@gmail.com>
2026-05-05 16:12:41 -0300

    boton grande

--- src/componets/GrillaReproductores.jsx
+++ src/componets/GrillaReproductores.jsx
@@ -113,7 +113,7 @@ const URL_API = import.meta.env.VITE_API_URL || "http://localhost:3001/canciones
           as={Link} 
           to={`/detalle/${cancion.id}`} 
           variant="outline-success" 
-          size="sm"
+          size=""
           className="text-decoration-none rounded-pill fw-bold mt-2 align-self-start" 
         >
           Ver Detalle
