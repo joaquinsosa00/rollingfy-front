a11eff428d2348a423031050f950288f24b3b78d
joaquinsosa00 <joaquin150496@gmail.com>
2026-05-06 19:16:13 -0300

    protector

--- src/componets/ProtectorAdmin.jsx
+++ src/componets/ProtectorAdmin.jsx
@@ -1,1 +1,13 @@
 ﻿
+import { Navigate } from "react-router-dom";
+
+const ProtectorAdmin = ({ children, usuarioLogueado }) => {
+  // Si el usuario no está logueado, lo redirigimos automáticamente al Login
+  if (!usuarioLogueado) {
+    return <Navigate to="/Login" />;
+  }
+  
+  return children;
+};
+
+export default ProtectorAdmin;
